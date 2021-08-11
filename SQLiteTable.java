c45022e335c361f89f1ab3fa7e269bc4332ee423
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-08-12 00:33:40 +0700

    commit

--- SQLiteTable.java
+++ SQLiteTable.java
@@ -16,14 +16,14 @@ import java.util.List;
     read more on MyLibSimpleSQLite
     https://github.com/gzeinnumer/MyLibSimpleSQLite
 */
-@SQLiteTable(tableName = "${TABLE_NAME}$")
+@SQLiteTable(tableName = "${TABLE_NAME}")
 public class ${CAP_NAME} extends SQLiteLIB<${CAP_NAME}> {
     @PrimaryKeyTypeData private int id;
     @VarcharTypeData    private String name;
 
     public ${CAP_NAME}() {}
 
-    //INSERT INTO ${TABLE_NAME}$ (name) VALUES ('Zein');
+    //INSERT INTO ${TABLE_NAME} (name) VALUES ('Zein');
     public boolean insert() {
         ${CAP_NAME} data = new ${CAP_NAME}();
         data.setName("Zein");
@@ -31,7 +31,7 @@ public class ${CAP_NAME} extends SQLiteLIB<${CAP_NAME}> {
         return insertData(${CAP_NAME}.class, GblVariabel.myDb, data);
     }
 
-    //UPDATE ${TABLE_NAME}$ SET name='Name Update' WHERE id='1';
+    //UPDATE ${TABLE_NAME} SET name='Name Update' WHERE id='1';
     public boolean update() {
         //set your value to update
         ${CAP_NAME} data = new ${CAP_NAME}();
@@ -46,35 +46,35 @@ public class ${CAP_NAME} extends SQLiteLIB<${CAP_NAME}> {
         return updatedData(${CAP_NAME}.class, GblVariabel.myDb, data, condition, fieldToUpdate);  // return true/false
     }
 
-    //DELETE FROM ${TABLE_NAME}$ WHERE id='1';
+    //DELETE FROM ${TABLE_NAME} WHERE id='1';
     public boolean delete() {
         String condition = "WHERE id='1'";
         return deleteData(${CAP_NAME}.class, GblVariabel.myDb, condition);
     }
 
-    //type 1 SELECT COUNT(*) FROM ${TABLE_NAME}$;
+    //type 1 SELECT COUNT(*) FROM ${TABLE_NAME};
     public int count() {
         return countData(${CAP_NAME}.class, GblVariabel.myDb);
     }
 
-    //type 2 SELECT COUNT(*) FROM ${TABLE_NAME}$ WHERE id='1';
+    //type 2 SELECT COUNT(*) FROM ${TABLE_NAME} WHERE id='1';
     public int count2() {
         String condition = "WHERE id='1'";
         return countData(${CAP_NAME}.class, GblVariabel.myDb, condition);
     }
 
-    // SELECT COUNT(id) FROM ${TABLE_NAME}$;
+    // SELECT COUNT(id) FROM ${TABLE_NAME};
     public int queryCount() {
-        String query = "SELECT COUNT(id) FROM ${TABLE_NAME}$;";
+        String query = "SELECT COUNT(id) FROM ${TABLE_NAME};";
         return queryCount(${CAP_NAME}.class, GblVariabel.myDb, query);
     }
 
-    //type 1 SElECT * FROM ${TABLE_NAME}$;
+    //type 1 SElECT * FROM ${TABLE_NAME};
     public List<${CAP_NAME}> read() {
         return readData(${CAP_NAME}.class, GblVariabel.myDb);
     }
 
-    //type 2 SELECT * FROM ${TABLE_NAME}$ WHERE id='1';
+    //type 2 SELECT * FROM ${TABLE_NAME} WHERE id='1';
     public List<${CAP_NAME}> read2() {
         String condition = "WHERE flag_active='1'";
 
@@ -82,12 +82,12 @@ public class ${CAP_NAME} extends SQLiteLIB<${CAP_NAME}> {
     }
 
     public List<${CAP_NAME}> query(){
-        String query ="SELECT ${TABLE_NAME}$.* FROM table1;";
+        String query ="SELECT ${TABLE_NAME}.* FROM table1;";
         return queryData(${CAP_NAME}.class, GblVariabel.myDb, query);
     }
 
     public boolean queryResultUpdate() {
-        String query = "UPDATE ${TABLE_NAME}$ SET name='name new' WHERE id='1'";
+        String query = "UPDATE ${TABLE_NAME} SET name='name new' WHERE id='1'";
         return queryResult(${CAP_NAME}.class, GblVariabel.myDb, query);
     }
 
